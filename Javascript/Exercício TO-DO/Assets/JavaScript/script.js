63133819fb93f2d2e6d8a6a78cd7c3b5cdf78078
iamThiagoo <thiagofer2003@outlook.com>
2021-07-14 23:33:12 -0300

    Exercício TO-DO LIST - finalizado a parte do JS

---
+++ Javascript/Exercício TO-DO/Assets/JavaScript/script.js
@@ -0,0 +1,74 @@
+const escreveTarefa = document.querySelector(".writeTasks");
+const btnAdiciona = document.querySelector("#addTasks");
+const tarefas = document.querySelector(".tasksToDo");
+
+function newTask(text) {
+    const li = createLi();
+    li.innerText = text;
+    tarefas.appendChild(li);
+    createButton(li)
+    limpaOInput();
+    saveTasks();
+}
+
+function createLi(){
+    const li = document.createElement("li");
+    return li;
+}
+
+function createButton(li) {
+    li.innerText += ` `;
+    const botaoApagaTarefa = document.createElement("button");
+    botaoApagaTarefa.innerText = `Apagar`;
+    botaoApagaTarefa.setAttribute("class", "apagar")
+    botaoApagaTarefa.setAttribute("title", "Apagar essa tarefa?")
+    li.appendChild(botaoApagaTarefa);
+}
+
+function limpaOInput(){
+    escreveTarefa.value = ` `;
+    escreveTarefa.focus();
+}
+
+function saveTasks(){
+    const AllLis = tarefas.querySelectorAll("li");
+    const TaskList = [];
+
+    for (let tarefinha of AllLis){
+        let tarefa = tarefinha.innerText;
+        tarefa = tarefa.replace("Apagar", "").trim();
+        TaskList.push(tarefa);
+    }
+
+    const tarefasJSON = JSON.stringify(TaskList);
+    localStorage.setItem("tarefas", tarefasJSON);
+}
+
+function addSaveTasks(){
+    const pegandoTarefas = localStorage.getItem("tarefas");
+    const lista = JSON.parse(pegandoTarefas);
+    for (let i of lista){
+        newTask(i);
+    }
+} addSaveTasks();
+
+btnAdiciona.addEventListener('click', function(){
+    if (!escreveTarefa.value) return;
+    newTask(escreveTarefa.value);
+});
+
+escreveTarefa.addEventListener("keypress", function(e){
+    if (e.keyCode === 13){
+        if (!escreveTarefa.value) return;
+        newTask(escreveTarefa.value); 
+    }
+});
+
+document.addEventListener("click", (e) => {
+    const element  = e.target;
+    
+    if(element.classList.contains("apagar")){
+        element.parentElement.remove();
+        saveTasks();
+    }
+});
